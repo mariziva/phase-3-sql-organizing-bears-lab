adc4bbcd042b71e3b2dae15cd9b32a0c79ef9259
Mari Taksa <welch.mari@gmail.com>
2021-08-18 16:03:00 -0500

    commit tree f710b235163ca4f8b61fcd9f826fd409fbb093ab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'purple', 'shitty tbh', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'blue', 'also a pain', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'periwinkle', 'absolute angel', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'F', 'brick red', 'v serious but we love her for it', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 14, 'F', 'brown', 'we hardly knew ye so idk', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'green', 'festive', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'candy apple red', 'boss bitch', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, 'M', 'transparent', 'a literal murderer so', 1);
